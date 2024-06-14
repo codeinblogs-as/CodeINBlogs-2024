1d18272b7ef66f59e53a38a699e7cd43dbb16b92
Anshul Soni <anshulsoni2022@gmail.com>
2024-06-14 19:07:58 +0530

    error-fixing dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class', 
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -39,6 +42,7 @@ const config: Config = {
       },
     },
   },
+
   plugins: [],
 };
 
