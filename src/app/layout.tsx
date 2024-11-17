31eb8c6b57538357bc21eead507011e6b0c3df1a
Anshul Soni <anshulsoni2022@gmail.com>
2024-11-17 14:04:19 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@
 import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
-import { AuthProvider } from '@/context/AuthContext'; // Import your context provider
+import { AuthProvider } from '@/context/AuthContext';
 import { ThemeProvider } from "next-themes";
+import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
   title: "CodeINBlogs",
@@ -16,14 +17,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const headersList = headers();
+  const pathname = headersList.get("x-pathname") || headersList.get("x-invoke-path") || "";
+  const isHomePage = pathname === "/";
+
   return (
     <html lang="en">
-      <body className={GeistSans.className}>
-
-
-                      {children}
-
-
+      <body className={`${GeistSans.className} ${isHomePage ? 'dark' : ''}`}>
+        <ThemeProvider attribute="class" defaultTheme={isHomePage ? 'dark' : 'light'}>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
